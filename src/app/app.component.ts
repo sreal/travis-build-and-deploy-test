bdb38656b31623a381cdf7e0f11200634ccae322
Simon Eames <simon.richard.eames@gmail.com>
2017-11-16 16:07:42 +1000

    Deploy gh pages with travis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Hollaback Bot';
+  teams = [
+    { name: "Pizza Roulette", ready: false },
+    { name: "Shared Space", ready: false },
+    { name: "Daisy Chain", ready: false },
+    { name: "Hearty Habits", ready: false },
+    { name: "Crowdhound", ready: false },
+    { name: "Hollaback Bot.", ready: false },
+    { name: "Dirt", ready: false },
+    { name: "Pegasus", ready: false },
+    { name: "Y@Voice", ready: false },
+    { name: "Flytly", ready: false }
+  ];
 }
